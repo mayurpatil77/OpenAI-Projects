8cdae47729acc6abeedf733c2781ec9171c6dcb5
Mayur <mrualpatil@gmail.com>
2023-08-04 11:44:19 +0530

    Improve Code Efficiency Tool
    Improve Code Efficiency Tool With The Help of OpenAI API

---
+++ Improve code efficiency/app.js
@@ -0,0 +1,47 @@
+const apiUrl = "https://api.openai.com/v1/chat/completions";
+const apiKey = "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";
+const codebtn = document.querySelector("#codebtn");
+const improve = document.querySelector("#improve");
+
+function getOptimisedCode() {
+  const inputText = document.querySelector("#input");
+  const input = inputText.value;
+
+  const userMessage = {
+    role: "user",
+    content: input,
+  };
+
+  const systemMessage = {
+    role: "system",
+    content: "You will be provided with a piece of code, and your task is to provide ideas for efficiency improvements.",
+  };
+
+  const params = {
+    messages: [systemMessage, userMessage], 
+    model: "gpt-3.5-turbo",
+    max_tokens: 500,
+    temperature: 0,
+  };
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + apiKey,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const improvedCode = data.choices[0]?.message?.content;
+      const improveCodeElement = document.querySelector("#improve-code");
+      improveCodeElement.textContent = improvedCode;
+      })
+    .catch((error) => {
+      const improveCodeElement = document.querySelector("#improve-code");
+      improveCodeElement.textContent = "Error occurred while fetching optimized code.";  
+    });
+}
+
+codebtn.addEventListener("click", getOptimisedCode);
